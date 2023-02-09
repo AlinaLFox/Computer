23f2ed7c58ec7893a1d7a48d29931fa0dc87ed30
AlinaLFox <124040276+AlinaLFox@users.noreply.github.com>
2023-02-09 19:44:49 +0200

    Initial commit

---
+++ src/edu/tekwill/academy/computer/Computer.java
@@ -0,0 +1,17 @@
+package edu.tekwill.academy.computer;
+
+import components.*;
+import peripherals.*;
+public class Computer {
+
+    HardDrive hd = new HardDrive();
+    Motherboard mb = new Motherboard();
+    Processor p = new Processor();
+    Monitor mon = new Monitor();
+    Mouse ms = new Mouse();
+    Keyboard kb = new Keyboard();
+
+
+
+}
+
